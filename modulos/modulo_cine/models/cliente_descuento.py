8bf9342ab729da3ce103b35588a088861132ae89
Angel Martin <i72marma@uco.es>
2019-05-10 18:27:30 +0200

    Finalizado desarrollo inicial de los modelos

--- modulos/modulo_cine/models/cliente_descuento.py
+++ modulos/modulo_cine/models/cliente_descuento.py
@@ -4,3 +4,16 @@ from odoo import models, fields, api
 
 class (models.Model):
 
+    _name = 'modulo_cine.cliente_descuento'
+
+    numero_butaca = fields.Many2one('modulo_cine.cliente', 'numero_butaca')
+    nombre = fields.Many2one('modulo_cine.cliente', 'nombre')
+    numero_sala = fields.Many2one('modulo_cine.cliente', 'numero_sala')
+    fecha = fields.Many2one('modulo_cine.cliente', 'fecha')
+    tipo = fields.Many2one('modulo_cine.descuento', 'tipo')
+
+    _sql_constraints = [
+        ('PK6',
+         'UNIQUE(numero_butaca, nombre, numero_sala, fecha)',
+         "El descuento por cliente es unico"),
+    ]
