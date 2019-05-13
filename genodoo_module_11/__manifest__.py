7223f6ae1eadd66e0d45f1280257fd3e783bb4ca
rexuswolf <francisjmp99@gmail.com>
2019-05-13 16:29:50 +0200

    Now it's responsivegit add genodoo_module_11/*

---
+++ genodoo_module_11/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "GenOdoo",
+
+    'summary': """
+       Importa tus estilos de páginas web mediante este módulo""",
+
+    'description': """
+       Mediante este módulo podemos importar templates como vistas en nuestro Sitio Web,
+       consiguiendo así transportar nuestros diseños de páginas web creadas con la herramienta
+       Drag & Drop que nos proporciona Odoo.
+       Además, dispone de dos plantillas, una para modificar la página inicial del Sitio Web
+       y otra para crear una nueva página con el estilo que queramos.
+    """,
+
+    'author': "rexuswolf",
+    'website': "github.com/rexuswolf",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Diseño',
+    'version': '1.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'depends': ['website'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/home.xml',
+        'views/newpage.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
