53a15dcbfad017706f70189c2fa4e9ca9987d6da
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-05-13 23:12:53 +0200

    Created reservation model

--- modulo_restaurante/models/models.py
+++ modulo_restaurante/models/models.py
@@ -2,14 +2,11 @@
 
 from odoo import models, fields, api
 
-# class viewtractor(models.Model):
-#     _name = 'viewtractor.viewtractor'
+ class reservation(models.Model):
+    _name = 'restaurant.reservation'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    email = fields.Char()
+    phone = fields.Integer()
+    date = fields.Datetime()
+    diners = fields.Integer()
