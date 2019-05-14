a9e8d0b386f24a8d6b62acaddb5aa8c3cd07adde
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-05-14 13:49:34 +0200

    Added datetime unification to controller, WIP database modification

--- modulo_restaurante/controllers/controllers.py
+++ modulo_restaurante/controllers/controllers.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from odoo import http, models
+from datetime import datetime
+
 
 class RestaurantController(http.Controller):
-    @http.route('/reservas/submit', type='http', auth='public', methods=['GET', 'POST'], website=True)
+    @http.route('/reservas/submit', type='http', auth='public', methods=['POST'], website=True)
     def make_reservation(self, **kw):
-            response = http.request.render('modulo_restaurante.thanks', lazy=False)
-            return response
+        month_translate = {'Enero':'01', 'Febrero':'02', 'Marzo':'03', 
+        'Abril':'04', 'Mayo':'05', 'Junio':'06', 'Julio':'07', 'Agosto':'08', 
+        'Septiembre':'09', 'Octubre':'10', 'Noviembre':'11', 'Diciembre':'12'}
+        date_str = ",".join([
+            str(kw['day']), 
+            month_translate[kw['month']], 
+            str(datetime.today().year),
+            kw['hour']])
+        date_obj = datetime.strptime(date_str, "%d,%m,%Y,%H:%M")
+        print(date_obj)
+        
+        response = http.request.render('modulo_restaurante.thanks', lazy=False)
+        return response
