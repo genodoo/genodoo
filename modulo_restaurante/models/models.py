039c955d16f4e8d6d7eaedfac39d60834aa50559
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-05-14 13:50:16 +0200

    Created wrapper for create() method

--- modulo_restaurante/models/models.py
+++ modulo_restaurante/models/models.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 
-class reservation(models.Model):
+class Reservation(models.Model):
     _name = 'restaurant.reservation'
 
     name = fields.Char()
@@ -10,3 +10,13 @@ class reservation(models.Model):
     phone = fields.Integer()
     date = fields.Datetime()
     diners = fields.Integer()
+
+    @api.multi
+    def make(name_, email_, phone_, date_, diners_):
+        return(self.create({
+            'name':name_,
+            'email':email_,
+            'phone':phone_,
+            'date':date_,
+            'diners':diners_
+        }))
