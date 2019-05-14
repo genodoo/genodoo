9e3eddb75cc8799d5c22db18dca86346618a221e
Adoen Lunnae <unshekdelmonton@gmail.com>
2019-05-14 12:32:40 +0200

    Created Thank You page for reservations

--- modulo_restaurante/controllers/controllers.py
+++ modulo_restaurante/controllers/controllers.py
@@ -2,8 +2,7 @@
 from odoo import http
 
 class RestaurantController(http.Controller):
-    @http.route('/reservas/submit', type='http', auth='public', methods=['POST'])
+    @http.route('/reservas/submit', type='http', auth='public', methods=['GET', 'POST'], website=True)
     def make_reservation(self, **kw):
-            response = http.HttpRequest.render('../views/thanks.xml', {})
-            print(response)
+            response = http.request.render('modulo_restaurante.thanks', lazy=False)
             return response
