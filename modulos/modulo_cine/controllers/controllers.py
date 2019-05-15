108645ce30e0b65a6364f8b396ee5f217c794281
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-05-15 15:24:04 +0200

    Controller added but template not showing

--- modulos/modulo_cine/controllers/controllers.py
+++ modulos/modulo_cine/controllers/controllers.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
-# class ModuloCine(http.Controller):
-#     @http.route('/modulo_cine/modulo_cine/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class PeliculasCine(http.Controller):
 
-#     @http.route('/modulo_cine/modulo_cine/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('modulo_cine.listing', {
-#             'root': '/modulo_cine/modulo_cine',
-#             'objects': http.request.env['modulo_cine.modulo_cine'].search([]),
-#         })
-
-#     @http.route('/modulo_cine/modulo_cine/objects/<model("modulo_cine.modulo_cine"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('modulo_cine.object', {
-#             'object': obj
-#         })
+    @http.route('/modulo_cine', type='http', auth='public', methods={'GET'}, website=True)
+    def list(self, **kw):
+        return http.request.render('modulo_cine.listing', {
+            'films': http.request.env['cine.pelicula'].search([]),
+        })
+        
