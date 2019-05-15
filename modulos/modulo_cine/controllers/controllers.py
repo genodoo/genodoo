24b95b7a8b985393760b1e0e7ced879a4a178b9d
Pablo Rodríguez Guillén <i72rogup@uco.es>
2019-05-15 15:36:28 +0200

    home page moved too '/'

--- modulos/modulo_cine/controllers/controllers.py
+++ modulos/modulo_cine/controllers/controllers.py
@@ -3,7 +3,7 @@ from odoo import http
 
 class PeliculasCine(http.Controller):
 
-    @http.route('/home', type='http', auth='public', methods={'GET'}, website=True)
+    @http.route('/', type='http', auth='public', methods={'GET'}, website=True)
     def cinema_page(self, **kw):
         return http.request.render('modulo_cine.Home', {
             'films': http.request.env['cine.pelicula'].search([])
