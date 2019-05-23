fca699395e4441331e0289bfe17352dccbb0c6fd
Pablorg99 <i72rogup@uco.es>
2019-05-23 18:19:23 +0200

    security updated, this time must be true

--- modulos/modulo_genodoo/__manifest__.py
+++ modulos/modulo_genodoo/__manifest__.py
@@ -22,7 +22,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/home.xml',
